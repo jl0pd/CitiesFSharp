e84daa6f1ef655487da4ab531958427541687dd9
Denis Proshutinskii <jl0pd@outlook.com>
2019-11-02 09:51:58 +0700

    New type definition replaced with String extensions

--- Program.fs
+++ Program.fs
@@ -1,15 +1,15 @@
 ﻿open System
 
-type Valid(content: string) =
-    member __.Str = content.ToLower()
-    member this.First = this.Str.[0]
-    member this.Last = this.Str.[content.Length - 1] 
-    member this.Successor (other: Valid) = this.First = other.Last
+type System.String with
+    member this.Lower = this.ToLower()
+    member this.First = this.Lower.[0]
+    member this.Last = this.Lower.[this.Length - 1] 
+    member this.Successor (other: String) = this.First = other.Last
 
 type City =
     | None
     | Used       of string
-    | Existing   of Valid
+    | Existing   of string
     | NotExisted of string
 
 let input (text: string) =
@@ -25,26 +25,26 @@ let getCity usedCities knownCities =
     match input "> " with
     | ""   -> None
     | city -> if   usedCities  |> contains <| city then Used city
-              elif knownCities |> contains <| city then Existing <| Valid city
+              elif knownCities |> contains <| city then Existing city
               else NotExisted city
 
 let citiesGame = fun availableCities ->
-    let rec loop = fun (lastCity: Valid) usedCities allCities ->
+    let rec loop = fun lastCity usedCities allCities ->
 
-        let nextTurn() = loop lastCity usedCities allCities
+        let tryAgain() = loop lastCity usedCities allCities
 
         match getCity usedCities allCities with
         | Existing city ->
             if city.Successor lastCity
-            then loop city (city.Str::usedCities) allCities
-            else printfn "Letters doesn't match"             ; nextTurn()
-        | None            -> printfn "Empty input"           ; nextTurn()
-        | Used city       -> printfn "%s city is used"  city ; nextTurn() 
-        | NotExisted city -> printfn "%s never existed" city ; nextTurn() 
+            then loop city (city.Lower::usedCities) allCities
+            else printfn "Letters doesn't match"             ; tryAgain()
+        | None            -> printfn "Empty input"           ; tryAgain()
+        | Used city       -> printfn "%s is used" city       ; tryAgain() 
+        | NotExisted city -> printfn "%s never existed" city ; tryAgain() 
 
     match availableCities with
-    | [] | [_]       -> printfn "Empty cities list"
-    | first :: other -> loop (Valid first) [first] other
+    | [] | [_]       -> printfn "Too few cities"
+    | first :: other -> loop first [first] other
 
 [<EntryPoint>]
 let main argv =
