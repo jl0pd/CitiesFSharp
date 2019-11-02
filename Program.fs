842b485cfac1f752ba48bea34c7d7692cc99d122
Denis Proshutinskii <jl0pd@outlook.com>
2019-11-02 14:33:28 +0700

    Added game against computer

--- Program.fs
+++ Program.fs
@@ -2,6 +2,7 @@
 
 type System.String with
     member this.Lower = this.ToLower()
+    member this.Title = (Char.ToUpper this.[0]).ToString() + this.Substring(1)
     member this.First = this.Lower.[0]
     member this.Last = this.Lower.[this.Length - 1] 
     member this.Successor (other: String) = this.First = other.Last
@@ -12,41 +13,104 @@ type City =
     | Existing   of string
     | NotExisted of string
 
-let input (text: string) =
+let str = function
+    | None -> ""
+    | Used city -> city
+    | Existing city -> city
+    | NotExisted city -> city
+
+type CitiesList = string list
+
+type Order =
+    | Player
+    | Computer
+    | Ended
+
+type GameState = {
+    Move: Order;
+    UsedCities: CitiesList;
+    KnownCities: CitiesList;
+    LastCity: City }
+
+let initState = function
+    | [] | [_] ->
+        printfn "Too few cities"
+        { Move = Ended
+          UsedCities = []
+          KnownCities = []
+          LastCity = None }
+    | (first: string) :: other ->
+        printfn "%s" first.Title
+        { Move = Player
+          UsedCities = [ first.Lower ]
+          KnownCities = List.map (fun (x:string) -> x.Lower) other
+          LastCity = Existing first.Lower }
+    
+let playerInput (text: string) =
     Console.Write(text)
     Console.ReadLine()
 
 let getCity usedCities knownCities =
     let contains (list: string list) (value: string) =
-        list
-        |> List.map (fun x -> x.ToLower())
-        |> List.contains (value.ToLower())
+        List.contains (value.ToLower()) list
 
-    match input "> " with
+    match playerInput "> " with
     | ""   -> None
     | city -> if   usedCities  |> contains <| city then Used city
               elif knownCities |> contains <| city then Existing city
               else NotExisted city
 
-let citiesGame = fun availableCities ->
-    let rec loop = fun lastCity usedCities allCities ->
+let rec playerMove (state: GameState) :GameState =
+    let usedCities = state.UsedCities
+    let allCities = state.KnownCities
+    let lastCity = state.LastCity
 
-        let tryAgain() = loop lastCity usedCities allCities
-
-        match getCity usedCities allCities with
+    let tryAgain() = playerMove state
+    match getCity usedCities allCities with
         | Existing city ->
-            if city.Successor lastCity
-            then loop city (city.Lower::usedCities) allCities
+            if city.Successor <| str lastCity
+            then { state with
+                    Move=Computer
+                    LastCity=Existing city
+                    UsedCities=(city::usedCities)
+                    KnownCities=List.filter ((<>)city) allCities }
             else printfn "Letters doesn't match"             ; tryAgain()
         | None            -> printfn "Empty input"           ; tryAgain()
         | Used city       -> printfn "%s is used" city       ; tryAgain() 
         | NotExisted city -> printfn "%s never existed" city ; tryAgain() 
 
-    match availableCities with
-    | [] | [_]       -> printfn "Too few cities"
-    | first :: other -> loop first [first] other
+let computerMove (state: GameState) :GameState =
+    let usedCities = state.UsedCities
+    let allCities = state.KnownCities
+    let lastCity = state.LastCity
+    
+    match List.tryFind (fun (x: string) -> x.StartsWith((str lastCity).Last)) allCities with
+    | Some city ->
+        printfn "%s" city.Title
+        { state with
+            Move = Player
+            LastCity = Existing city
+            UsedCities = city::usedCities
+            KnownCities = List.filter ((<>)city) allCities }
+    | option.None ->
+        printfn "No citites left for this letter"
+        { state with Move=Ended }
+
+
+let citiesGame = fun (availableCities: string list) ->
+    let rec loop = fun (state: GameState) ->
+        match state with
+        | { Move = Player } -> 
+            loop <| playerMove state
+        | { Move = Computer } ->
+            loop <| computerMove state
+        | { Move = Ended } ->
+            ()
+    initState availableCities |> loop
 
 [<EntryPoint>]
 let main argv =
-    citiesGame ["Novosibirsk"; "Kemerovo"; "Orenburg"]
+    System.IO.File.ReadAllLines "cities.txt"
+    |> List.ofArray
+    |> citiesGame
     0 // return an integer exit code
