e5acc3a1f5d3f94699be43660d29e82b2ca94fd8
Denis Proshutinskii <jl0pd@outlook.com>
2019-11-06 23:08:45 +0700

    Code made more functional

--- Program.fs
+++ Program.fs
@@ -1,23 +1,30 @@
 ﻿open System
 
-type System.String with
-    member this.Lower = this.ToLower()
-    member this.Title = (Char.ToUpper this.[0]).ToString() + this.Substring(1)
-    member this.First = this.Lower.[0]
-    member this.Last = this.Lower.[this.Length - 1] 
-    member this.Successor (other: String) = this.First = other.Last
-
 type City =
     | None
-    | Used       of string
-    | Existing   of string
-    | NotExisted of string
+    | Used        of string
+    | Existing    of string
+    | NotExisting of string
 
-let str = function
-    | None -> ""
-    | Used city -> city
-    | Existing city -> city
-    | NotExisted city -> city
+let lowerCase (str: string) =
+    str.ToLower()
+
+let titleCase (str: string) =
+    (Char.ToUpper str.[0]).ToString() + str.Substring(1)
+
+let firstLetter (str: string) =
+    str.[0]
+
+let lastLetter (str: string) =
+    str.[str.Length - 1]
+
+let startsWith (letter: char) (str: string) =
+    str.StartsWith letter
+
+let successor s1 s2 =
+    match s1, s2 with
+    | city, lastCity when firstLetter city = lastLetter lastCity -> true
+    | _ -> false
 
 type CitiesList = string list
 
@@ -27,10 +34,10 @@ type Order =
     | Ended
 
 type GameState = {
-    Move: Order;
-    UsedCities: CitiesList;
-    KnownCities: CitiesList;
-    LastCity: City }
+    Move: Order
+    UsedCities: CitiesList
+    KnownCities: CitiesList
+    LastCity: string }
 
 let initState = function
     | [] | [_] ->
@@ -38,67 +45,66 @@ let initState = function
         { Move = Ended
           UsedCities = []
           KnownCities = []
-          LastCity = None }
+          LastCity = "" }
     | (first: string) :: other ->
-        printfn "%s" first.Title
+        titleCase first |> printfn "%s"
         { Move = Player
-          UsedCities = [ first.Lower ]
-          KnownCities = List.map (fun (x:string) -> x.Lower) other
-          LastCity = Existing first.Lower }
+          UsedCities = [ lowerCase first ]
+          KnownCities = List.map lowerCase other
+          LastCity = lowerCase first }
     
 let playerInput (text: string) =
     Console.Write(text)
-    Console.ReadLine()
+    lowerCase <| Console.ReadLine()
 
 let getCity usedCities knownCities =
-    let contains (list: string list) (value: string) =
-        List.contains (value.ToLower()) list
+    let contains list value =
+        list |> List.contains value
 
     match playerInput "> " with
     | ""   -> None
     | city -> if   usedCities  |> contains <| city then Used city
-              elif knownCities |> contains <| city then Existing city
-              else NotExisted city
+              elif knownCities |> contains <| city then Existing <| city
+              else NotExisting city
 
-let rec playerMove (state: GameState) :GameState =
-    let usedCities = state.UsedCities
-    let allCities = state.KnownCities
-    let lastCity = state.LastCity
+let useCity city state =
+    let { UsedCities = usedCities
+          KnownCities = allCities } = state
+    { state with
+            LastCity = city
+            UsedCities = city::usedCities
+            KnownCities = List.filter ((<>)city) allCities }
 
+let rec playerMove state =
+    let { LastCity = lastCity
+          UsedCities = usedCities  
+          KnownCities = allCities } = state
+    
     let tryAgain() = playerMove state
     match getCity usedCities allCities with
         | Existing city ->
-            if city.Successor <| str lastCity
-            then { state with
-                    Move=Computer
-                    LastCity=Existing city
-                    UsedCities=(city::usedCities)
-                    KnownCities=List.filter ((<>)city) allCities }
+            if successor city lastCity
+            then { useCity city state with Move = Computer }
             else printfn "Letters doesn't match"             ; tryAgain()
-        | None            -> printfn "Empty input"           ; tryAgain()
-        | Used city       -> printfn "%s is used" city       ; tryAgain() 
-        | NotExisted city -> printfn "%s never existed" city ; tryAgain() 
+        | None             -> printfn "Empty input"          ; tryAgain()
+        | Used city        -> printfn "%s is used" city      ; tryAgain() 
+        | NotExisting city -> printfn "%s not existing" city ; tryAgain() 
 
-let computerMove (state: GameState) :GameState =
-    let usedCities = state.UsedCities
-    let allCities = state.KnownCities
-    let lastCity = state.LastCity
-    
-    match List.tryFind (fun (x: string) -> x.StartsWith((str lastCity).Last)) allCities with
+let computerMove state =
+    let { KnownCities = allCities
+          LastCity = lastCity
+        } = state
+
+    match List.tryFind (startsWith <| lastLetter lastCity) allCities with
     | Some city ->
-        printfn "%s" city.Title
-        { state with
-            Move = Player
-            LastCity = Existing city
-            UsedCities = city::usedCities
-            KnownCities = List.filter ((<>)city) allCities }
+        titleCase city |> printfn "%s"
+        { useCity city state with Move = Player }
     | option.None ->
         printfn "No citites left for this letter"
-        { state with Move=Ended }
+        { state with Move = Ended }
 
-
-let citiesGame = fun (availableCities: string list) ->
-    let rec loop = fun (state: GameState) ->
+let citiesGame = fun availableCities ->
+    let rec loop = fun state ->
         match state with
         | { Move = Player } -> 
             loop <| playerMove state
